03005f6efd5cf6b9ef2ee35e7d6b84b4ef2f6d07
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-05-15 14:04:38 +0200

    Updated setup.py for astropy virtualenvs

--- virtualenvs/astropy.dev/setup.py
+++ virtualenvs/astropy.dev/setup.py
@@ -1,59 +1,83 @@
 import os
+import sys
+import urllib2
+import subprocess
+from copy import deepcopy
+from distutils import version
 
-#NUMPY_VERSIONS = ['1.4.1', '1.5.0', '1.5.1', '1.6.0', '1.6.1', '1.6.2']
-NUMPY_VERSIONS = ['1.7.0']
-#PYTHON_VERSIONS = ['2.6', '2.7', '3.1', '3.2', '3.3']
-PYTHON_VERSIONS = ['3.3']
-SCIPY_VERSIONS = [None]
-#SCIPY_VERSIONS = ['0.11.0']
+NUMPY_VERSIONS = ['1.4.1', '1.5.0', '1.5.1', '1.6.0', '1.6.1', '1.6.2', '1.7.0', '1.7.1']
+PYTHON_VERSIONS = ['2.6', '2.7', '3.1', '3.2', '3.3']
+
+NUMPY_URL = "https://pypi.python.org/packages/source/n/numpy/numpy-{nv}.tar.gz"
+
+def download(url):
+    print "Downloading " + url
+    u = urllib2.urlopen(url)
+    f = open(os.path.basename(url), 'wb')
+    f.write(u.read())
+    f.close()
+    
+for nv in NUMPY_VERSIONS:
+    if not os.path.exists('numpy-{nv}.tar.gz'.format(nv=nv)):
+        download(NUMPY_URL.format(nv=nv))
+
+
+SCRIPT = """
+# Create virtual environment
+$HOME/usr/python/bin/virtualenv-{pv} --python=$HOME/usr/python/bin/python{pv} --no-site-packages {full}
+
+# Install Cython
+{full}/bin/pip-{pv} install Cython
+
+# Unpack Numpy to temporary directory
+mkdir {full}.tmp
+tar -C {full}.tmp -xvzf numpy-{nv}.tar.gz
+
+# Change to directory
+cd {full}.tmp/numpy-{nv}/
+
+# Build Numpy
+# FFLAGS="-m64" ../{full}/bin/python{pv} setup.py build --fcompiler=gnu95'.format(**versions)
+CC={cc} ../../{full}/bin/python{pv} setup.py build
+../../{full}/bin/python{pv} setup.py install
+
+# Back to start
+cd ../../
+
+# Remove temporary directory
+rm -r {full}.tmp
+"""
+
+
+def setup_virtualenv(versions):
+
+    print versions['full']
+
+    f = open('{full}.log'.format(**versions), 'wb')
+    code = subprocess.call(SCRIPT.format(**versions), shell=True, stdout=f, stderr=f)
+    f.close()
+
+all_versions = []
 
 versions = {}
 for numpy_version in NUMPY_VERSIONS:
     versions['nv'] = numpy_version
-    for scipy_version in SCIPY_VERSIONS:
-        versions['sv'] = scipy_version
-        if scipy_version is not None:
+    for python_version in PYTHON_VERSIONS:
+        if python_version in ['3.1', '3.2', '3.3']:
             if numpy_version == '1.4.1':
                 continue
-        for python_version in PYTHON_VERSIONS:
-            if python_version in ['3.1', '3.2', '3.3']:
-                if numpy_version == '1.4.1':
-                    continue
-            if python_version in ['3.3']:
-                if numpy_version != '1.7.0':
-                    continue
-            versions['pv'] = python_version
-            versions['full'] = 'python{pv}-numpy{nv}'.format(**versions)
+        if python_version in ['3.3']:
+            if numpy_version != '1.7.0':
+                continue
+        versions['pv'] = python_version
+        versions['full'] = 'python{pv}-numpy{nv}'.format(**versions)
+        if version.LooseVersion(numpy_version) >= version.LooseVersion("1.6.2"):
+            versions['cc'] = 'clang'
+        else:
+            versions['cc'] = 'gcc'
 
-            print versions['full']
+        all_versions.append(deepcopy(versions))
 
-            if scipy_version is not None:
-                versions['full'] += '-scipy{sv}'.format(**versions)
-
-            os.system('$HOME/usr/python/bin/virtualenv-{pv} --python=$HOME/usr/python/bin/python{pv} --no-site-packages {full}'.format(**versions))
-
-            # If using Python 3.3, need to update pip to the git version
-            if python_version == '3.3':
-                os.system('cd pip.git ; rm -r build ; ../{full}/bin/python{pv} setup.py install ; cd ..'.format(**versions))
-
-            os.system('{full}/bin/pip-{pv} install cython'.format(**versions))
-
-            os.system('{full}/bin/pip-{pv} install PyFFTW3'.format(**versions))
-
-            os.system('tar xvf tarfiles/numpy-{nv}.tar'.format(**versions))
-            os.chdir('numpy-{nv}'.format(**versions))
-            os.system('FFLAGS="-m64" ../{full}/bin/python{pv} setup.py build --fcompiler=gnu95'.format(**versions))
-            os.system('../{full}/bin/python{pv} setup.py install'.format(**versions))
-            os.chdir('../')
-            os.system('rm -r numpy-{nv}'.format(**versions))
-
-            if scipy_version is not None:
-
-                os.system('tar xvf tarfiles/scipy-{sv}.tar'.format(**versions))
-                os.chdir('scipy-{sv}'.format(**versions))
-                os.system('FFLAGS="-m64"  ../{full}/bin/python{pv} setup.py build --fcompiler=gnu95'.format(**versions))
-                os.system('../{full}/bin/python{pv} setup.py install'.format(**versions))
-                os.chdir('../')
-                os.system('rm -r scipy-{sv}'.format(**versions))
-
-
+from multiprocessing import Pool
+p = Pool(processes=24)
+p.map(setup_virtualenv, all_versions)
